beb2654b80014a5a3b596a599fa640a01a00d3b3
Victor Seleimend <sseleimend@gmail.com>
2025-07-26 18:03:13 -0300

    Update auth controller by adding signup controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,13 +1,62 @@
 import { type Request, type Response } from "express";
+import bcrypt from "bcryptjs";
 
-export const signup = (req: Request, res: Response) => {
-  res.send("signup route");
+import User, { type UserSchema } from "../models/user.model.ts";
+import { genJWT } from "../lib/utils.ts";
+
+type AuthReq<T = {}> = Request<{}, {}, T>;
+
+export const signup = async (req: AuthReq<UserSchema>, res: Response) => {
+  const { email, fullName, password, profilePic } = req.body;
+  try {
+    if (!email || !fullName || !password)
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+
+    if (password.length < 6)
+      return res.status(400).json({
+        message: "Password must be at least 6 characters",
+      });
+
+    const user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (!newUser)
+      return res.status(400).json({
+        message: "Invalid user data",
+      });
+
+    genJWT(newUser._id, res);
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error in signup controller", error.message);
+  }
 };
 
-export const login = (req: Request, res: Response) => {
+export const login = (req: AuthReq, res: Response) => {
   res.send("login route");
 };
 
-export const logout = (req: Request, res: Response) => {
+export const logout = (req: AuthReq, res: Response) => {
   res.send("logout route");
 };
