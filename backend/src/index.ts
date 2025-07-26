e860953b0597c40d97f20eca76555e7a939e87c0
Victor Seleimend <sseleimend@gmail.com>
2025-07-26 19:45:24 -0300

    Update index.js by adding cookie-parser middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.ts";
 import { connectDb } from "./lib/db.ts";
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
