6826b8f1446e6b4acc31797227ccc1271b2b0db6
Victor Seleimend <sseleimend@gmail.com>
2025-07-27 10:26:46 -0300

    Add get messages functionality

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { ReqWithUser } from "../middlewares/auth.middleware";
 import User from "../models/user.model";
+import { Message } from "../models/message.model";
 
 export const getUsersForSidebar = async (req: Request, res: Response) => {
   try {
@@ -13,7 +14,29 @@ export const getUsersForSidebar = async (req: Request, res: Response) => {
     res.status(200).json(filteredUsers);
   } catch (error) {
     if (error instanceof Error)
-      console.log("Error in signup controller", error.message);
+      console.log("Error in getUsersForSidebar controller", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = (req as ReqWithUser).user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error in getMessages controller", error.message);
     return res.status(500).json({
       message: "Internal server error",
     });
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { getUsersForSidebar } from "../controllers/message.controller.ts";
+import {
+  getUsersForSidebar,
+  getMessages,
+} from "../controllers/message.controller.ts";
 import { protectRoute } from "../middlewares/auth.middleware.ts";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
