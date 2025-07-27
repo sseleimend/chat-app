3a686fdc8bc7f70755bf0c631fa6044a347e0474
Victor Seleimend <sseleimend@gmail.com>
2025-07-27 10:18:54 -0300

    Update index.ts by adding message routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.ts";
+import messageRoutes from "./routes/message.route.ts";
 import { connectDb } from "./lib/db.ts";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
