4c37e1c9e6d8e55499eb185368dd71eeb390d06d
Victor Seleimend <sseleimend@gmail.com>
2025-07-27 11:44:06 -0300

    Add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 function App() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   );
 }
 
