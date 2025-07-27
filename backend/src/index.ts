c9798b3083bc83c6cda107b5cdf3029e57e3f32c
Victor Seleimend <sseleimend@gmail.com>
2025-07-27 12:44:14 -0300

    Update index.ts by adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.ts";
 import messageRoutes from "./routes/message.route.ts";
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
